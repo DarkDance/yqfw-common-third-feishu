6a6c946ac8f8e0e653dc8b4ce2fe22a684f7cdd1
DarkDance <57419153@qq.com>
2025-09-28 10:21:47 +0800

    简化配置处理

--- src/main/java/cn/jzyunqi/common/third/feishu/FeishuAuth.java
+++ src/main/java/cn/jzyunqi/common/third/feishu/FeishuAuth.java
@@ -1,5 +1,6 @@
 package cn.jzyunqi.common.third.feishu;
 
+import cn.jzyunqi.common.utils.StringUtilPlus;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -23,16 +24,16 @@ public class FeishuAuth {
     public FeishuAuth(String appId, String appSecret) {
         this.appId = appId;
         this.appSecret = appSecret;
-        this.verificationToken = "";
-        this.encryptKey = "";
+        this.verificationToken = StringUtilPlus.EMPTY;
+        this.encryptKey = StringUtilPlus.EMPTY;
         this.wsConnect = Boolean.TRUE;
     }
 
     public FeishuAuth(String appId, String appSecret, String verificationToken, String encryptKey, Boolean wsConnect) {
         this.appId = appId;
         this.appSecret = appSecret;
-        this.verificationToken = verificationToken;
-        this.encryptKey = encryptKey;
+        this.verificationToken = StringUtilPlus.defaultString(verificationToken);
+        this.encryptKey = StringUtilPlus.defaultString(encryptKey);
         this.wsConnect = wsConnect;
     }
 }
